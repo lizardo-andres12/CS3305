4139d4207eba0352ddf868f4b441b1bc8b97f9ef
Lizardo Hernandez <lizardo.andres15@outlook.com>
2024-08-22 18:16:09 -0400

    added documentation to LinkedList.java

--- assignment3/LinkedList.java
+++ assignment3/LinkedList.java
@@ -34,6 +34,12 @@ public class LinkedList {
     //======== Your part to complete for this assignment =========
 
     //method #1: add first node
+    /**
+     * Adds data to the beginning of the list. If list is empty, head and tail pointers are initialized
+     * to the new node with the data given
+     *
+     * @param data The value of the new node in the list
+     */
     public void addFirstNode(int data) {
         if (head == null) {
             head = tail = new Node(data);
@@ -46,22 +52,28 @@ public class LinkedList {
     }
 
     //method #2: add node at index
+
+    /**
+     * Adds data at the index provided as argument. Indexes outside of range 0 to size-1 will not add the
+     * data and will print an error message to the console
+     *
+     * @param index The index where to add the new node in the list
+     * @param data The value of the new node in the list
+     */
     public void addAtIndex(int index, int data) {
         int numNodes = countNodes();
         if (index < 0 || index >= numNodes) {
             System.out.println("Invalid index, try again");
             return;
+        } else if (head == null || index == 0) {
+            addFirstNode(data);
+        } else if (index == numNodes - 1) {
+            addLastNode(data);
         }
 
         Node node = new Node(data);
         Node left = head;
         Node right = head.next;
-
-        if (head == null || index == 0) {
-            addFirstNode(data);
-        } else if (index == numNodes - 1) {
-            addLastNode(data);
-        }
 
         for (int i = 1; i < index; i++) {
             left = left.next;
@@ -73,6 +85,11 @@ public class LinkedList {
     }
 
     //method #3: remove first node
+    /**
+     * Removes the node pointed to by {@code head} and advances the head pointer to the next node.
+     * If list is empty, print an error message to the console. If the list has one element, set
+     * {@code head} and {@code tail} pointers to null
+     */
     public void removeFirstNode() {
         if (head == null) {
             System.out.println("List is empty");
@@ -87,8 +104,13 @@ public class LinkedList {
     }
 
     //method #4: remove last node
+    /**
+     * Removes the tail node. The list must be traversed to the node before {@code tail} to remove tail.
+     * If the list is empty, print an error message to the console. If there's only one element in the
+     * list, set {@code head} and {@code tail} to null
+     */
     public void removeLastNode() {
-        if (tail == null) {
+        if (head == null) {
             System.out.println("List is empty");
             return;
         } else if (head.next == null) {
@@ -106,6 +128,14 @@ public class LinkedList {
     }
 
     //method #5: remove node at index
+    /**
+     * Removes node at specific index. If list is empty, print an error message to the console. If the
+     * index is not in the range of 0 to size-1, print and error message to the console. Use two
+     * temporary pointers to know when the target index is found and the node before is found to do
+     * the insert
+     *
+     * @param index The 0-based index where the new node should be
+     */
     public void removeAtIndex(int index) {
         int numNodes = countNodes();
         if (head == null) {
@@ -130,6 +160,11 @@ public class LinkedList {
     }
 
     //method #6: countNodes
+    /**
+     * Traverses the list and totals the number of nodes linked in the list
+     *
+     * @return The number of nodes linked from head to tail
+     */
     public int countNodes() {
         int listSize = 0;
 
@@ -139,11 +174,17 @@ public class LinkedList {
            cur = cur.next;
         }
 
-        //this methods returns the list size
+        // this methods returns the list size
         return listSize;
     }
 
     //method #7: printInReverse  (Recursive method)
+    /**
+     * Recursively iterates through the list until the input {@code Node} is null. Once the base case is
+     * reached, print the values in returning time
+     *
+     * @param L The first node of the list to be printed
+     */
     public void printInReverse(Node L) {
        if (L != null) {
           printInReverse(L.next);
